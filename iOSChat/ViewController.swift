2f623312041d9935d966e3c93bcfdb0c98b026a4
Jean Paull <Jean Paull>
2021-03-20 14:53:40 -0300

    Initial Commit

---
+++ iOSChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSChat
+//
+//  Created by Jean Paul Borges Manzini on 20/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
